a6f0b37cb57b72a067631aff61addab3791345fe
MiNeves00 <61327611+MiNeves00@users.noreply.github.com>
2020-10-22 16:54:18 +0100

    Game Loop done and ends if necessary

--- src/gameLogic.pl
+++ src/gameLogic.pl
@@ -1,3 +1,4 @@
+/**Start Game */
 gameStart('Player','Player'):-
     write('Starting Player vs Player game...'),
     nl,
@@ -5,7 +6,8 @@ gameStart('Player','Player'):-
    initializePieces(InitialMap,InitializedMap),
     write('Map has been initialized...'),
     nl,
-    printMap(InitializedMap).
+    printMap(InitializedMap),
+    gameLoop(InitializedMap,'Player','Player').
 
 gameStart('Player','Com'):- %To Do
     write('Starting Player vs Com game...'),
@@ -23,6 +25,7 @@ gameStart('Com','Com'):- %To Do
     nl,
     printMap(InitialMap).
 
+/**Initialize Map */
 initializePieces(InitialMap,InitializedMap):-
     changeValueInMap(InitialMap,0,4,blackRing,InitialMap2),
     changeValueInMap(InitialMap2,0,3,blackRing,InitialMap3),
@@ -31,3 +34,51 @@ initializePieces(InitialMap,InitializedMap):-
     changeValueInMap(InitialMap4,4,0,whiteRing,InitialMap5),
     changeValueInMap(InitialMap5,3,0,whiteRing,InitialMap6),
     changeValueInMap(InitialMap6,4,1,whiteRing,InitializedMap).
+
+/**Game Loop */
+gameLoop(Map,'Player','Player'):-
+    whiteMoves(Map),
+    checkIfWin(Map,HasWon),
+    (HasWon = 'None' ->  
+        blackMoves(Map),
+        checkIfWin(Map,HasWon2),
+        (HasWon2 = 'None' ->
+            gameLoop(Map,'Player','Player')
+            ; 
+            won(HasWon2)
+        )
+        ;
+        won(HasWon)
+    )
+    .
+
+/**Check Win*/
+checkIfWin(Map,HasWon):- %Updates HasWon If Someone Wins
+    nl,
+    write('Checking if win'),
+    read(Input),
+    winInput(Input,HasWon).
+
+winInput(0,HasWon):- HasWon='None'.
+winInput(1,HasWon):- HasWon='B'.
+winInput(2,HasWon):- HasWon='W'.
+
+won('W'):-
+    nl,
+    write('White wins').
+
+won('B'):-
+    nl,
+    write('Black wins').
+
+
+/**Turn Move*/
+whiteMoves(Map):-
+    write('White moves...').
+
+blackMoves(Map):-
+    write('Black moves...').
+
+handleMove(_Input).
+
+
