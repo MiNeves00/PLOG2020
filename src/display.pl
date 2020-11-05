934f009b3ad5cda88f290fbc6a927047efb98df3
zemmacaes <jose.macaes.2000@gmail.com>
2020-11-05 17:10:29 +0000

    Each space now is a stack

--- src/display.pl
+++ src/display.pl
@@ -1,47 +1,45 @@
 %initial(-GameState)
 initial([
-[empty,empty,empty,blackBase,blackBase],
-[empty,empty,empty,empty,blackBase],
-[empty,empty,empty,empty,empty],
-[whiteBase,empty,empty,empty,empty],
-[whiteBase,whiteBase,empty,empty,empty]
+[[],[],[],[blackBall | [blackRing | [blackBase | []]]],[blackBall | [blackRing | [blackBase | []]]]],
+[[],[],[],[],[blackBall | [blackRing | [blackBase | []]]]],
+[[],[],[],[],[]],
+[[whiteBall | [whiteRing | [whiteBase | []]]],[],[],[],[]],
+[[whiteBall | [whiteRing | [whiteBase | []]]],[whiteBall | [whiteRing | [whiteBase | []]]],[],[],[]]
 ]).
 
 /**Intermediate game state example*/
 intermediateMap([
-[empty,empty,empty,blackBase,blackBall],
-[empty,empty,empty,blackBall,blackBase],
-[whiteBall,empty,whiteRing,empty,blackBall],
-[whiteBase,empty,whiteBall,empty,blackRing],
-[whiteBase,whiteBall,empty,empty,empty]
+[[],[],[],blackBase,blackBall],
+[[],[],[],blackBall,blackBase],
+[whiteBall,[],whiteRing,[],blackBall],
+[whiteBase,[],whiteBall,[],blackRing],
+[whiteBase,whiteBall,[],[],[]]
 ]).
 
 /**End game state example*/
 endMap([
-[empty,empty,blackBall,whiteBall,whiteBall],
-[empty,empty,empty,whiteRing,whiteBall],
-[empty,empty,whiteRing,blackBall,blackBall],
-[whiteBase,empty,empty,empty,blackRing],
-[whiteBase,whiteBase,empty,empty,empty]
+[[],[],blackBall,whiteBall,whiteBall],
+[[],[],[],whiteRing,whiteBall],
+[[],[],whiteRing,blackBall,blackBall],
+[whiteBase,[],[],[],blackRing],
+[whiteBase,whiteBase,[],[],[]]
 ]).
-
-/**Row to letter correspondence*/
-%rowLetter(+RowNum, -Letter)
-rowLetter(1, LE) :- LE='A'.
-rowLetter(2, LE) :- LE='B'.
-rowLetter(3, LE) :- LE='C'.
-rowLetter(4, LE) :- LE='D'.
-rowLetter(5, LE) :- LE='E'.
 
 /**Atom to letter correspondence*/
 %character(+Atom, -Character)
 character(empty,C):- C=' . '.
+character([],C):- C=' . '.
 character(whiteRing,C):- C=' W '.
 character(whiteBall,C):- C='(W)'.
 character(whiteBase,S):- S='WoW'.
 character(blackRing,C):- C=' B '.
 character(blackBall,C):- C='(B)'.
 character(blackBase,S):- S='BoB'.
+character(1, C) :- C='A'.
+character(2, C) :- C='B'.
+character(3, C) :- C='C'.
+character(4, C) :- C='D'.
+character(5, C) :- C='E'.
 
 /**Display Game*/
 %display_game(+GameState)
@@ -55,9 +53,9 @@ display_game(X) :-
 printMatrix([],6).
 
 printMatrix([H|T],N):-
-    rowLetter(N,LE),
+    character(N,C),
     write('   '),
-    write(LE), %Writes the row letter before each row
+    write(C), %Writes the row letter before each row
     write('   |  '),
     N1 is N + 1,
     printRow(H),
@@ -67,7 +65,13 @@ printMatrix([H|T],N):-
 printRow([]).
 
 printRow([H|T]):-
-    character(H, C),
+    top(H, P),
+    character(P, C),
     write(C), %Writes each character in its space
     write('  |  '),
     printRow(T).
+
+%top(+L, -H)
+top([], empty).
+
+top([H | T], H).
